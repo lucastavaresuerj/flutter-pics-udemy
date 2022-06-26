ffd84756604581571a62e85415e84afa4f11bf30
Lucas Tavares <lucastavares@localhost.localdomain>
2022-06-26 18:36:20 -0300

    Refactor to make statefull

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
 
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => print('Hi There!'),
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
           child: const Icon(Icons.add),
         ),
         appBar: AppBar(
